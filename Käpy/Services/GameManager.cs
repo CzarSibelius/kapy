2d420e4148ba5a42858ed07dbee89391a22836c6
Jari Randelin <jari.randelin@gmail.com>
2021-04-11 15:56:45 +0300

    State loading removed from GameManager

--- Käpy/Services/GameManager.cs
+++ Käpy/Services/GameManager.cs
@@ -8,33 +8,23 @@ namespace Käpy.Services
 {
     public class GameManager : IGameManager
     {
-        private readonly IGameStateStorageService gameStateStorageService;
-        public GameState GameState { get; }
-
-        public GameManager(IGameStateStorageService gameStateStorageService)
-        {
-            this.gameStateStorageService = gameStateStorageService;
-
-            GameState = this.gameStateStorageService.GetState();
-        }
-
-        public GameState State => GameState;
+        public GameState State { get; set; }
 
         public void AddResource(string resourceName, int amount)
         {
-            GameState.Resources[resourceName] += amount;
+            State.Resources[resourceName] += amount;
             var resource = ResourceConfig.Get(resourceName);
 
             resource.BuildRequirements?
                 .Where(c => c is ResourceCost)
                 .Cast<ResourceCost>()
                 .ToList()
-                .ForEach(c => GameState.Resources[c.Name] -= c.Amount);
+                .ForEach(c => State.Resources[c.Name] -= c.Amount);
         }
 
         public void Research(Technology technology)
         {
-            GameState.ResearchedTechnologies.Add(technology);
+            State.ResearchedTechnologies.Add(technology);
         }
     }
 }
--- Käpy/Services/IGameManager.cs
+++ Käpy/Services/IGameManager.cs
@@ -1,10 +1,11 @@
 ﻿using Käpy.Business;
+using System.Threading.Tasks;
 
 namespace Käpy.Services
 {
     public interface IGameManager
     {
-        GameState State { get; }
+        GameState State { get; set; }
         void Research(Technology technology);
         void AddResource(string resource, int amount);
     }
